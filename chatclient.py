8bd0946b361568def3a01f7047788a63b70a3b55
camikaze99 <48164146+camikaze99@users.noreply.github.com>
2019-09-06 12:40:11 +0200

    Create chatclient.py

---
+++ chatclient.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+import sys
+import chatserver as chatServer
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connections = []
+
+class Client:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def sendMessage(self):
+        while True:
+            self.sock.send(bytes(input(""), 'utf-8'))
+#            print(str(a[0]) + ':' + str(a[1]), " said this")
+
+    def __init__(self, address):
+        self.sock.connect((address, 10000))
+
+        iThread = threading.Thread(target = self.sendMessage)
+        iThread.deamon = True
+        iThread.start()
+
+        while True:
+            data = self.sock.recv(1024)
+            if not data:
+                break
+            print(str(data, 'utf-8'))
+
+if (len(sys.argv) > 1):
+    client = Client(sys.argv[1])
+else:
+    server = chatServer.Server()
+    server.run()
